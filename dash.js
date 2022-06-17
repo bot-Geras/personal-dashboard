2890b64e1134d83dcb06990db27f8ed2b4c6f51e
Brian <briandave771@gmail.com>
2022-06-17 04:30:09 +0300

    added crypto currencies

--- dash.js
+++ dash.js
@@ -1,25 +1,51 @@
 /**
  * Challenge: get a random image from Unsplash and set it as the background
- * 
+ *
  * Part 1:
- * 
+ *
  * URL: https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature
  * (You can change the "query" at the end to whatever theme you want)
- * 
- * Change the body's backgroundImage to: 
+ *
+ * Change the body's backgroundImage to:
  * `url(<insert the URL of the iamge from the API here>)`
- * 
+ *
  * (You may need to dig around the response body a bit to find this URL)
- * 
+ *
  * (Note I've already added some CSS to resize the image within the window.
- * Instructions for this were found on CSS Tricks: 
+ * Instructions for this were found on CSS Tricks:
  * https://css-tricks.com/perfect-full-page-background-image/#awesome-easy-progressive-css3-way)
  */
 
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
-    .then(res => res.json())
-    .then(data => {
+fetch(
+  "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
+)
+  .then((res) => res.json())
+  .then((data) => {
+    document.body.style.backgroundImage = `url(${data.urls.regular})`;
+    document.getElementById("author").textContent = `${data.user.name}`;
+  })
+  .catch((err) => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
+)`;
+    document.getElementById("author").textContent = `By: Dodi Achmad`;
+  });
 
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
-        document.getElementById("author").textContent = `${data.user.name}`
-    })
+fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+  .then((res) => {
+    if (!res.ok) {
+      throw Error("Something is wrong");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    document.getElementById("crypto-top").innerHTML = `
+            <img src="${data.image.small}" alt="">
+            <span>${data.name}</span>
+        `;
+
+    document.getElementById("crypto").innerHTML += `
+            <p>🎯: $${data.market_data.current_price.usd}</p>
+            <p>👆: $${data.market_data.high_24h.usd}</p>
+            <p>👇: $${data.market_data.low_24h.usd}</p>
+        `;
+  });
